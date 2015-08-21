2fe3f50187f82ad4f3ff878fe92ef49d3cc9b26d
Federico Borgnia <fborgnia@gmail.com>
2015-08-20 22:41:51 -0300

    read actual values

--- server/sensors/dht11.py
+++ server/sensors/dht11.py
@@ -1,19 +1,19 @@
-# import Adafruit_DHT
+import Adafruit_DHT
 
 def read():
     ''' returns tuple of temperature and humidity '''
-    # return = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 2)
+    humidity, temperature = Adafruit_DHT.read_retry(Adafruit_DHT.DHT11, 2)
     metric = (11,14)
     metric = {
         "name": 'dht11',
         "measurements":[
             {
                 "name":"humidity",
-                "value":14
+                "value":humidity
             },
             {
                 "name":"temperature",
-                "value":11
+                "value":temperature
             }
         ]
 
