e021e0990eeaa00e92fb796b9add6a7f938f997f
Mauricio Miranda <maurimiranda@gmail.com>
2015-09-17 20:43:30 -0300

    Fix agenda mongo config

--- app/agenda.js
+++ app/agenda.js
@@ -1,7 +1,7 @@
 var config = require('../config/config'),
   glob = require('glob'),
   Agenda = require('agenda'),
-  agenda = new Agenda(config.mongo);
+  agenda = new Agenda({db:{address: config.db}});
 
 var jobs = glob.sync(config.root + '/app/jobs/*.js');
 jobs.forEach(function (job) {
