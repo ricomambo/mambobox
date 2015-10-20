700c3c204cc55c3a9a8fa4bdd6edcb5f591995a1
Mauricio Miranda <maurimiranda@gmail.com>
2015-10-20 16:39:27 -0300

    Remove mongoose

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 var express = require('express'),
   config = require('./config/config'),
   glob = require('glob'),
-  mongoose = require('mongoose'),
   agenda = require('./app/agenda'),
   morgan = require('morgan'),
   methodOverride = require('method-override');
 
-mongoose.connect(config.db);
-var db = mongoose.connection;
-db.on('error', function () {
-  throw new Error('unable to connect to database at ' + config.db);
-});
-
-var models = glob.sync(config.root + '/app/models/*.js');
-models.forEach(function (model) {
-  require(model);
-});
-
 var app = express();
-app.use(morgan('tiny'));
+app.use(morgan('dev'));
 app.use(methodOverride('_method'));
 
 require('./config/express')(app, config);
