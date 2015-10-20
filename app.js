66883361ed038a6043ed08f66cdaea0479794cea
Mauricio Miranda <maurimiranda@gmail.com>
2015-10-20 18:14:01 -0300

    Remove morgan

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ var express = require('express'),
   config = require('./config/config'),
   glob = require('glob'),
   agenda = require('./app/agenda'),
-  morgan = require('morgan'),
   methodOverride = require('method-override');
 
 var app = express();
-app.use(morgan('dev'));
 app.use(methodOverride('_method'));
 
 require('./config/express')(app, config);
