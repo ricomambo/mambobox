80b17ee436db0a635dfe13d91d005853596455f8
Mauricio Miranda <maurimiranda@gmail.com>
2015-12-31 18:21:05 -0300

    Use env config

--- config/config.js
+++ config/config.js
@@ -1,46 +1,11 @@
 var path = require('path'),
     rootPath = path.normalize(__dirname + '/..'),
-    env = process.env.NODE_ENV || 'development';
+    env = process.env.NODE_ENV || 'development',
+    config = require('./' + env + '.json');
 
-var config = {
-  development: {
-    root: rootPath,
-    app: {
-      name: 'mambobox'
-    },
-    port: 3001,
-    db: 'mongodb://localhost/mambobox-development',
-    influx: {
-      host: 'localhost',
-      database: 'mambobox-development'
-    }
-  },
-
-  test: {
-    root: rootPath,
-    app: {
-      name: 'mambobox'
-    },
-    port: 3000,
-    db: 'mongodb://localhost/mambobox-test',
-    influx: {
-      host: 'localhost',
-      database: 'mambobox'
-    }
-  },
-
-  production: {
-    root: rootPath,
-    app: {
-      name: 'mambobox'
-    },
-    port: 3333,
-    db: 'mongodb://localhost/mambobox-production',
-    influx: {
-      host: 'localhost',
-      database: 'mambobox'
-    }
-  }
+config.root = rootPath;
+config.app = {
+  name: 'mambobox'
 };
 
-module.exports = config[env];
+module.exports = config;
