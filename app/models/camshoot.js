b3595d6e4821e1ac39e63461165f7dd39fe07d92
Mauricio Miranda <maurimiranda@gmail.com>
2016-01-20 17:51:16 -0300

    Use fake camera when real one is not available

--- app/models/camshoot.js
+++ app/models/camshoot.js
@@ -1,9 +1,18 @@
-var Camera = process.env.NODE_ENV === 'production' ?
-    require('../../lib/camera') :
-    require('../../lib/fake-camera');
+var Camera = require('../../lib/camera'),
+    FakeCamera = require('../../lib/fake-camera');
 
 var Camshoot = function () {
-  this.cam = new Camera();
+  if (process.env.NODE_ENV === 'production') {
+    try {
+      this.cam = new Camera();
+    } catch(error) {
+      console.log(error);
+    }
+  }
+
+  if (!this.cam) {
+    this.cam = new FakeCamera();
+  }
 };
 
 Camshoot.prototype.capture = function (callback) {
